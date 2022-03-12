a218f701b4a7881c53512253ac34de924b6a159b
jguerrero <jguerrero@playvox.com>
2022-03-12 10:39:14 -0500

    fix comment

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //App para hallar el cuadrado y el cubo de un número
-//Se lanza y se ejecuta con http://localhost:8080 con aws
+//Se lanza y se ejecuta con http://localhost:8080 con aws y pipeline
 @SpringBootApplication
 @RestController
 public class ProtobootappApplication {
